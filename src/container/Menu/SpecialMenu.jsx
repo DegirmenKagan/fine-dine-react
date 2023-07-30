1db071942b249fcf849279d3c7cd0ce2ff76c99d
Kağan Değirmen <kagandegirmen@gmail.com>
2023-07-30 18:56:39 +0300

    SpecialMenu codeReview typo fixes

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,13 +7,14 @@ import "./SpecialMenu.css";
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
-      <SubHeading title={"Menu That Fits Your Palette"} />
+      <SubHeading title="Menu That Fits Your Palette" />
       <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
+
     <div className="app__specialMenu-menu">
       <div className="app__specialMenu-menu_wine  flex__center">
         <p className="app__specialMenu-menu_heading">Wine & Beer</p>
-        <div className="app__specialMenu-menu_items">
+        <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
             <MenuItem
               key={wine.title + index}
@@ -24,12 +25,14 @@ const SpecialMenu = () => (
           ))}
         </div>
       </div>
+
       <div className="app__specialMenu-menu_img">
-        <img src={images.menu} alt="menu img" />
+        <img src={images.menu} alt="menu__img" />
       </div>
+
       <div className="app__specialMenu-menu_cocktails  flex__center">
         <p className="app__specialMenu-menu_heading">Cocktails</p>
-        <div className="app__specialMenu-menu_items">
+        <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
             <MenuItem
               key={cocktail.title + index}
@@ -41,11 +44,11 @@ const SpecialMenu = () => (
         </div>
       </div>
     </div>
-    <div style={{ marginTop: "15px" }}>
+
+    <div style={{ marginTop: 15 }}>
       <button type="button" className="custom__button">
         View More
       </button>
-      t
     </div>
   </div>
 );
