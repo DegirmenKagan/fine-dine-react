dcaf47a1c43b5ab05b6433f3d0a613915ab05d64
Kağan Değirmen <kagandegirmen@gmail.com>
2023-08-04 00:34:56 +0300

    Footer container implemented

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
-import {FiFacebook,FiTwitter,FiInstagram} from 'react-icons/fi'
-
-import {FooterOverlay, Newsletter} from '../../components'
-import './Footer.css';
+import React from "react";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { images } from "../../constants";
+import { FooterOverlay, Newsletter } from "../../components";
+import "./Footer.css";
 
 const Footer = () => (
-  <div className='app__footer section__padding'>
+  <div className="app__footer section__padding">
     <FooterOverlay />
-    <Newsletter/>
+    <Newsletter />
 
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">12 W 21st St, New York, NY</p>
+        <p className="p__opensans">+1 123-123-123</p>
+        <p className="p__opensans">+1 321-321-321</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <h3 className="app__navbar-title">Fine Dine Restaurant</h3>
+        {/* <img src={} alt="logo"/> */}
+        <p className="p__opensans">
+          "The best way to find yourself is to lose yourself in the service of
+          others."
+        </p>
+        <img
+          src={images.spoon}
+          alt="spoon"
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">
+        2023 Fine Dine Restaurant. No rights reserved.
+      </p>
+    </div>
   </div>
 );
 
 export default Footer;
- 
